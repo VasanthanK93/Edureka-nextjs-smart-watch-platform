469e9ead655713a4d04648cd1fbf502e69d4782a
Vasanthan K <vasanthank29@gmail.com>
2023-12-10 23:07:35 +0530

    Login page Implementation

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,84 @@
 'use client'
 
-export default function LoginPage(){
+import { useState } from "react"
+import { useRouter } from 'next/navigation';
 
-    
+export default function LoginPage() {
 
-    return "Login";
+    const [isProcessing, setIsProcessing] = useState(false);
+    const [loginFormData, setLoginFormData] = useState({
+        email: "",
+        password: ""
+    })
+    const { push } = useRouter();
+
+    async function handleSubmit(e: any) {
+        setIsProcessing(true);
+        e.preventDefault();
+        try {
+            const response = await fetch("http://localhost:4800/login", {
+                method: "POST",
+                headers: {
+                    "content-type": "application/json; charset=UTF-8"
+                },
+                body: JSON.stringify(loginFormData)
+            });
+            const data = await response.json();
+            window.localStorage.setItem("access-token", data.accessToken)
+            setIsProcessing(false);
+            cleanUpForm();
+            push('/');
+        }
+        catch (error) {
+            console.log(error);
+            setIsProcessing(false);
+        }
+
+    }
+
+    function cleanUpForm() {
+        setLoginFormData({ email: "", password: "" })
+    }
+
+    function handleChange(e: any) {
+        setLoginFormData({ ...loginFormData, [e.target.name]: e.target.value })
+    }
+
+    return <div className="container mx-auto p-4">
+        <h1 className="text-xl mb-4">User Login</h1>
+        <form className="space-y-6" onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="email" className="block text-sm font-medium leading-6 text-white">Email Address</label>
+                <div className="mt-2">
+                <input id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email" 
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                   value={loginFormData.email} onChange={handleChange} />
+                </div>
+            </div>
+            <label htmlFor="password" className="block text-sm font-medium leading-6 text-white">
+                  Password
+                </label>
+                <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                value={loginFormData.password} onChange={handleChange} />
+            </div>
+            <button disabled={isProcessing} type="submit" className="flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 bg-indigo-600 hover:bg-indigo-500">Login</button>
+        </form>
+        <p className="mt-10 text-center text-sm text-gray-500">
+            New member?{' '}
+            <a href="/register" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+              Register Here
+            </a>
+          </p>
+    </div>
 }
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -43,16 +43,39 @@ export default function RegisterPage() {
 
     return <div className="container mx-auto p-4">
         <h1 className="text-xl mb-4">User Registration</h1>
-        <form onSubmit={handleSubmit}>
+        <form className="space-y-6" onSubmit={handleSubmit}>
             <div>
-                <label className="block">Email</label>
-                <input className="w-full p-2 border rounded" type="text" id="email" name="email" value={registerFormData.email} onChange={handleChange} />
+                <label htmlFor="email" className="block text-sm font-medium leading-6 text-white">Email Address</label>
+                <div className="mt-2">
+                <input id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email" 
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                   value={registerFormData.email} onChange={handleChange} />
+                </div>
             </div>
-            <div>
-                <label className="block">Password</label>
-                <input className="w-full p-2 border rounded" type="text" id="password" name="password" value={registerFormData.password} onChange={handleChange} />
+            <label htmlFor="password" className="block text-sm font-medium leading-6 text-white">
+                  Password
+                </label>
+                <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                value={registerFormData.password} onChange={handleChange} />
             </div>
-            <button disabled={isProcessing} type="submit" className={`text-white px-4 py-2 rounded ${isProcessing ? 'bg-gray-800' : 'bg-blue-500 hover:bg-blue-600'}`}>Register User</button>
+            <button disabled={isProcessing} type="submit" className="flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 bg-indigo-600 hover:bg-indigo-500">Register User</button>
         </form>
+        <p className="mt-10 text-center text-sm text-gray-500">
+            already a member?{' '}
+            <a href="/login" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+              login to App Here
+            </a>
+          </p>
     </div>
 }

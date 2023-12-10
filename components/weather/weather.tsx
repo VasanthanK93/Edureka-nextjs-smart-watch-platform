10332408628384fa01bc6fb767e148233f6294ea
Vasanthan K <vasanthank29@gmail.com>
2023-12-11 02:04:05 +0530

    weather update changes

--- components/weather/weather.tsx
+++ components/weather/weather.tsx
@@ -3,14 +3,14 @@ export default async function Weather() {
 
     const OPENWEATHER_API_KEY = process.env['OPENWEATHER_API_KEY']
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=28.7041&lon=77.1025&appid=${OPENWEATHER_API_KEY}`, { next: { revalidate: 300 } });
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=53.4631&lon=2.2913&appid=${OPENWEATHER_API_KEY}`, { next: { revalidate: 300 } });
         const data = await response.json();
         console.log(data);
         if (data.cod === 401) {
             return <div>-- C</div>
         }
         else {
-            return <div className="text-white">34</div>
+            return(<div className="text-white">{`Weather in ${data.name}: ${data.weather[0].description}, ${Math.round(data.main.temp - 273.15)}°C `}</div>) 
         }
 
     } catch (error) {
